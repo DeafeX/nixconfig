68d1611dcb968f1e02f1444ea02d14eb7a430796
Deafex <thalesmyfortes@gmail.com>
2025-02-25 01:24:15 +0000

    cool

--- devShells.nix
+++ devShells.nix
@@ -1,19 +1,12 @@
 {
-  __functor = (self: x: {
-    inputs = self.inputs;
-    outputs = {...}@inputs: {
-      default = (self.outputs inputs).${x.name}.overrideAttrs (x.derivation inputs);
-    };
-  });
-  extension = {}; 
   inputs = {
-    nixpkgs= import <nixpkgs>;
+    nixpkgs = import <nixpkgs>;
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = {nixpkgs, rust-overlay }:
+  outputs = { nixpkgs, rust-overlay }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -23,11 +16,15 @@
       toolchain = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
         toolchain.default.override {
           extensions = [ "rust-src" "cargo" "clippy" "rust-analyzer" ];
-        });
-    in {
-      rust = with pkgs; mkShell {
-        buildInputs = [ pkg-config toolchain ];
-        shellHook = ["nushell"];
+        }
+      );
+    in
+    {
+      devShells.${system} = {
+        rust = with pkgs; mkShell {
+          buildInputs = with pkgs; [ pkg-config toolchain ];
+          shellHook = [ "nushell" ];
+        };
       };
     };
 }
