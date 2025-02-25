31d795206ffd436a831f50cbf1af8bc507475d55
Deafex <thalesmyfortes@gmail.com>
2025-02-25 16:38:51 +0000

    aaa

--- devShells.nix
+++ devShells.nix
@@ -1,30 +1,31 @@
-{
-  inputs = {
-    nixpkgs = import <nixpkgs>;
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      nixpkgs.follows = "nixpkgs";
+let system = "x86_64-linux";
+in  rec {
+  mkShell = { names, extraInputs ? [], overlays ? [] }:
+  {
+    inputs = shells.rust.inputs;
+    outputs = {nixpkgs, ...}@inputs:
+    let
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+    in {
+      devShells.${system}.default = builtins.listToAttrs (builtins.map (x: {name = x; value = pkgs.mkShell shells.${x}.shellArg pkgs;}) names);
     };
   };
-  outputs = { nixpkgs, rust-overlay }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ (import rust-overlay) ];
-      };
-      toolchain = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
-        toolchain.default.override {
-          extensions = [ "rust-src" "cargo" "clippy" "rust-analyzer" ];
-        }
-      );
-    in
-    {
-      devShells.${system} = {
-        rust = with pkgs; mkShell {
-          buildInputs = with pkgs; [ pkg-config toolchain ];
-          shellHook = [ "nushell" ];
+  shells = {
+    rust = rec {
+      inputs = {
+        rust-overlay = {
+          url = "github:oxalixa/rust-overlay";
+          nixpkgs.follows = "nixpkgs";
         };
       };
+      shellArg = pkgs: {
+          buildInputs = with pkgs; [
+            pkg-config
+                      ];
+          shellHook = "nu";
+        };
     };
+  };
 }
